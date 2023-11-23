2132174aa85c18e871e98f2a79ce52a94c6f4f8a
Zicio <zicio@yandex.ru>
2023-11-23 16:11:15 +0400

    8

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,30 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostsList from "../components/postsList/PostsList";
 import PostForm from "../components/postForm/PostForm";
 import { IFilter, IPost } from "../types";
 import MyModal from "../components/UI/modal/MyModal";
 import MyButton from "../components/UI/button/MyButton";
 import { usePosts } from "../hooks/usePosts";
-import PostFilter from "../components/PostFilter/postFilter";
+import PostFilter from "../components/postFilter/PostFilter";
+import { PostService } from "../API/PostService";
+import Loader from "../components/UI/loader/Loader";
 
 const MainPage = () => {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Bost 1",
-      body: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eu iaculis aliquet, nisl nisl aliquet nisl, eu iaculis nisl nisl euismod nisl.",
-    },
-    {
-      id: 2,
-      title: "Dost 2",
-      body: "Borem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eu iaculis aliquet, nisl nisl aliquet nisl, eu iaculis nisl nisl euism",
-    },
-    {
-      id: 3,
-      title: "Cost 3",
-      body: "Aorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eu iaculis aliquet, nisl nisl aliquet nisl, eu iaculis nisl nisl euawawdawdawdawdawdawdawdawd",
-    },
-  ]);
+  const [posts, setPosts] = useState<IPost[]>([]);
 
   const [filter, setFilter] = useState<IFilter>({
     sort: "",
@@ -33,11 +19,23 @@ const MainPage = () => {
 
   const [isModal, setIsModal] = useState<boolean>(false);
 
+  const [isPostsLoading, setIsPostsLoading] = useState<boolean>(false);
+
   const sortedAndSearchedPosts = usePosts({ posts, ...filter });
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost: IPost) => {
     setPosts([...posts, newPost]);
     setIsModal(false);
+  };
+
+  const fetchPosts = async () => {
+    setIsPostsLoading(true);
+    setPosts(await PostService.getAll());
+    setIsPostsLoading(false);
   };
 
   const deletePost = (id: number) => {
@@ -46,6 +44,15 @@ const MainPage = () => {
 
   return (
     <>
+      <MyButton
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          return fetchPosts();
+        }}
+      >
+        Get Posts
+      </MyButton>
       <MyButton type="button" onClick={() => setIsModal(true)}>
         Создать пост
       </MyButton>
@@ -53,11 +60,23 @@ const MainPage = () => {
         <PostForm create={createPost} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostsList
-        posts={sortedAndSearchedPosts}
-        remove={deletePost}
-        title="Список постов"
-      />
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostsList
+          posts={sortedAndSearchedPosts}
+          remove={deletePost}
+          title="Список постов"
+        />
+      )}
     </>
   );
 };
